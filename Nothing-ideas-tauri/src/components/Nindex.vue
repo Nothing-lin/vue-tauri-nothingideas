9794c25b07a6e56c3b4de4b9d336d7127bae59f5
WIN-J854I48S38V\Nothinglin <1353695200@qq.com>
2024-04-09 23:14:06 +0800

    完成Nindex首页的开发

---
+++ Nothing-ideas-tauri/src/components/Nindex.vue
@@ -0,0 +1,49 @@
+/** 首页 */
+<template>
+    <div class="common-layout">
+        <el-container>
+            <el-header>Header</el-header>
+
+
+            <el-main>
+                <el-card style="max-width: 70vw; margin: auto;">
+                    <!-- 页头 -->
+                    <template #header>
+                        <div class="card-header" style="display: flex; justify-content: space-between; align-items: center;">
+                            <span class="header-title">nothingIdeas想法流程</span>
+                            <el-button-group class="btn-group">
+                                <el-button type="primary" size="mini">新增</el-button>
+                                <el-button type="danger" size="mini">删除</el-button>
+                            </el-button-group>
+                        </div>
+                    </template>
+
+                    <!-- 内容  -->
+                    <template v-for="n in 10">
+                        <div class="item" style="margin-top: 15px; margin-bottom: 15px;">
+                            <el-button plain size="mini">Button 1</el-button>
+                            <el-button plain :type="n%2 === 0? 'danger' : 'success'" size="mini">Button 2</el-button>
+                            <span class="text" style="margin-left: 10px;">Text</span>
+                        </div>
+                    </template>
+
+                    <!-- 页脚 -->
+                    <template #footer>
+                        
+                        <div style="display: flex; justify-content: space-between; align-items: center;">
+                            <el-pagination background layout="prev, pager, next" :total="1000" />
+
+                            <el-button-group class="btn-group">
+                                <el-button type="primary" size="mini" plain>全部</el-button>
+                                <el-button type="success" size="mini" plain>已闭环</el-button>
+                                <el-button type="warning" size="mini" plain>未闭环</el-button>
+                            </el-button-group>
+
+                        </div>
+                    </template>
+                </el-card>
+            </el-main>
+        </el-container>
+    </div>
+</template>
+  
--- Nothing-ideas-tauri/src/router.js
+++ Nothing-ideas-tauri/src/router.js
@@ -1,10 +1,16 @@
 import { createRouter,createWebHashHistory } from "vue-router";
 import One from "./components/One.vue"
 import Two from "./components/Two.vue"
+import Nindex from "./components/Nindex.vue"
+
 
 const router  = createRouter({
     history:createWebHashHistory(),
     routes:[
+        {
+            path:"/",
+            component:Nindex
+        },
         {
             path:"/One",
             component:One
