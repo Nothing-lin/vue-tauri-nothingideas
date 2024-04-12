e8f9cfc4b69cdc8b475bdf12bfaae668d56d77f6
WIN-J854I48S38V\Nothinglin <1353695200@qq.com>
2024-04-12 18:44:14 +0800

    实现节点的【更新】【删除】

--- Nothing-ideas-tauri/src/components/Detail.vue
+++ Nothing-ideas-tauri/src/components/Detail.vue
@@ -12,8 +12,8 @@
         <el-button-group class="btn-group">
           <el-button type="primary" size="mini" plain style="box-shadow: none;"
             @click="dialogFormVisible = true">新增节点</el-button>
-          <el-button type="success" size="mini" plain style="box-shadow: none;">流程闭环</el-button>
-          <el-button type="warning" size="mini" plain style="box-shadow: none;">恢复流程</el-button>
+          <el-button type="success" size="mini" plain style="box-shadow: none;border-color: none;" :disabled="this.projectstaus === '已闭环'" @click="updateProjectStatus()">流程闭环</el-button>
+          <el-button type="warning" size="mini" plain style="box-shadow: none;border-color: none;" :disabled="this.projectstaus === '未闭环'" @Click="recoverProjectStatus()">恢复流程</el-button>
           <el-button size="mini" plain style="box-shadow: none;" @click="fetchPreoject()">测试按钮{{ project_id
           }}</el-button>
         </el-button-group>
@@ -27,28 +27,27 @@
               <div class="block">
                 <el-timeline>
                   <!-- node节点列表 -->
-                  <el-timeline-item timestamp="2018年4月12日" placement="top" v-for="item in NothingProjectNodes">
+                  <el-timeline-item timestamp="2018年4月12日" placement="top" v-for="item in NothingProjectNodes" :key="item.node_id">
                     <template v-slot:dot>
                       <!-- 使用 dot 插槽自定义小圆点 -->
                       <span class="custom-dot" :style="{ backgroundColor: item.node_type === '拓展' ? 'red' : 'none' }">{{
                         item.node_type }}</span>
                     </template>
                     <el-card class="custom-card">
-                      <el-row :gutter="20">
-                        <el-col :span="16">
+                      <el-row :gutter="23">
+                        <el-col :span="19">
                           <div class="grid-content bg-purple">
                             <h3 style="margin-top: 0px;">{{ item.node_title }}</h3>
                             <!-- <p>{{ item.node_text }}</p> -->
                             <div v-html="item.node_text"></div>
                           </div>
                         </el-col>
-                        <el-col :span="8">
-                          <div class="grid-content bg-purple">
-                            <h4>参考网站</h4>
+                        <el-col :span="4">
+                          <div class="grid-content bg-purple" v-if="this.projectstaus === '未闭环'">
+                            <h4>操作</h4>
                             <ul>
-                              <li><el-link type="success">参考链接1</el-link></li>
-                              <li><el-link type="success">参考链接2</el-link></li>
-                              <li><el-link type="success">参考链接3</el-link></li>
+                              <li><el-link type="primary" @click="getNodeData(item.node_type,item.node_title,item.node_text,item.node_id)">编辑</el-link></li>
+                              <li><el-link type="danger" @click="deleteNode(item.node_id)">删除</el-link></li>
                             </ul>
                           </div>
                         </el-col>
@@ -83,9 +82,38 @@
         </el-form>
         <template #footer>
           <div class="dialog-footer">
-            <el-button @click="dialogFormVisible = false">取消</el-button>
+            <el-button @click="dialogFormVisible = false" @Click="clearForm()">取消</el-button>
             <el-button type="primary" @click="addNode()">
               新增
+            </el-button>
+          </div>
+        </template>
+      </el-dialog>
+
+
+      <!-- 编辑-对话框 -->
+      <el-dialog v-model="dialogFormEditVisible" title="编辑节点" width="800">
+        <el-form :model="formEdit">
+          <div style="display: flex;align-items: center;margin-bottom: 20px;">
+            <span style="width: 100px">节点标题：</span>
+            <el-input v-model="formEdit.name" autocomplete="off" />
+          </div>
+
+          <div style="display: flex;align-items: center;margin-bottom: 20px;">
+            <span style="width: 100px">节点类型：</span>
+            <el-select v-model="typeEdit_value" placeholder="Select" style="width: 240px;">
+              <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value" />
+            </el-select>
+          </div>
+          <!-- <QuillEditor theme="snow" /> -->
+          <quill-editor ref="Editeditor" v-model="editorContent2" :options="editorOption" @text-change="handleTextChange"></quill-editor>
+
+        </el-form>
+        <template #footer>
+          <div class="dialog-footer">
+            <el-button @click="dialogFormEditVisible = false" @Click="clearForm2()">取消</el-button>
+            <el-button type="primary" @click="updateNode()">
+              更新
             </el-button>
           </div>
         </template>
@@ -114,7 +142,9 @@ export default {
       project_id: null,
       NothingProjectNodes: [],
       NothingProject: [],
+      node_item:{'node_type':'','node_title':'','node_text':'','node_id':''},
       projectname: '',
+      projectstaus: '',
       editorOption: {
         // 富文本编辑器配置选项
         placeholder: '请输入内容...', // 占位符
@@ -137,6 +167,7 @@ export default {
    await this.fetchPreojectNodes();
    this.NothingProject = await this.fetchPreoject();
    this.projectname = this.NothingProject[0].project_title;
+   this.projectstaus = this.NothingProject[0].project_status;
    console.log(this.projectname)
   },
   async mounted() {
@@ -178,6 +209,7 @@ export default {
       this.form.name = ''
       this.editorContent = ''
       this.type_value = ''
+      this.$refs.editor.setHTML('')
       const content = this.editorContent;
       const content2 = this.$refs.editor.getHTML();
       console.log(content)
@@ -187,11 +219,85 @@ export default {
     handleTextChange(newContent, oldContent, source) {
       this.editorContent = newContent;
     },
+    // 更新nothing_project表的project_status字段
+    async updateProjectStatus() {
+      const db = await Database.load("sqlite:NothingIdeas.db")
+      const query = `UPDATE nothing_project SET project_status = '已闭环' WHERE project_id = ${this.project_id}`
+      const result = await db.execute(query);
+      console.log(result)
+      await db.close();
+      this.projectstaus = '已闭环'
+    },
+    // 恢复流程
+    async recoverProjectStatus() {
+      const db = await Database.load("sqlite:NothingIdeas.db")
+      const query = `UPDATE nothing_project SET project_status = '未闭环' WHERE project_id = ${this.project_id}`
+      const result = await db.execute(query);
+      console.log(result)
+      await db.close();
+      this.projectstaus = '未闭环'
+    },
+    // 删除节点
+    async deleteNode(nodeid) {
+      const db = await Database.load("sqlite:NothingIdeas.db")
+      const query = `DELETE FROM nothing_project_node WHERE node_id = ${nodeid}`
+      const result = await db.execute(query);
+      console.log(result)
+      await db.close();
+      this.fetchPreojectNodes();
+
+    },
+    // 清空表单数据
+    clearForm() {
+      this.form.name = ''
+      this.editorContent = ''
+      this.type_value = ''
+      this.$refs.editor.setHTML('')
+    },
+    clearForm2() {
+      this.form.name = ''
+      this.editorContent = ''
+      this.type_value = ''
+      this.$refs.Editeditor.setHTML('')
+    },
+    // 获取节点数据
+    getNodeData(nodeType,nodeTitle,nodeText,nodeid) {
+      this.node_item.node_type = nodeType;
+      this.node_item.node_title = nodeTitle;
+      this.node_item.node_text = nodeText;
+      this.node_item.node_id = nodeid;
+      console.log(this.node_item)
+      this.dialogFormEditVisible = true;
+      
+      this.formEdit.name = nodeTitle;
+      this.typeEdit_value = nodeType;
+      this.$refs.Editeditor.setHTML(nodeText);
+
+    },
+    // 编辑按钮更新数据
+    async updateNode() {
+      const db = await Database.load("sqlite:NothingIdeas.db")
+
+      const query = `UPDATE nothing_project_node SET node_title = ?, node_text = ?, node_type = ? WHERE node_id = ?`
+      const params = [this.formEdit.name, this.$refs.Editeditor.getHTML(), this.typeEdit_value, this.node_item.node_id]
+      const result = await db.execute(query, params);
+      console.log(this.node_item.node_id)
+      await db.close();
+      this.fetchPreojectNodes();
+      this.dialogFormEditVisible = false;
+      // // 清空表单数据
+      this.formEdit.name = ''
+      this.editorContent2 = ''
+      this.typeEdit_value = ''
+      this.$refs.Editeditor.setHTML('')
+      this.node_item={};
+    }
   },
   setup() {
 
     // 新增按钮-对话框
     const dialogFormVisible = ref(false)
+    const dialogFormEditVisible = ref(false)
     const formLabelWidth = '140px'
 
     const Nodetextarea = ref('')
@@ -205,6 +311,17 @@ export default {
       type: [],
       resource: '',
       desc: '',
+    })
+
+    const formEdit = reactive({
+      name: '',
+      region: '',
+      date1: '',
+      date2: '',
+      delivery: false,
+      type: [],
+      resource: '',
+      desc: ''
     })
     const value = ref('')
 
@@ -220,9 +337,11 @@ export default {
     ]
 
     return {
+      dialogFormEditVisible,
       dialogFormVisible,
       formLabelWidth,
       form,
+      formEdit,
       Nodetextarea,
       options,
       value
